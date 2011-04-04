cd6738d8b44165062ff16027958b3780be62e3c6
johnnyoffline <johnnyoffline@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2011-04-04 22:28:42 +0000

    yeahhh
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheExtraSlotsPlugin/trunk@3516 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- web/js/aMapSlot.js
+++ web/js/aMapSlot.js
@@ -7,14 +7,14 @@ function aMapSlotConstructor()
 		{
 		  var script = document.createElement("script");
 		  script.type = "text/javascript";
-		  script.src = "http://maps.google.com/maps/api/js?sensor=false&callback=initialize";
+		  script.src = "http://maps.google.com/maps/api/js?sensor=false&callback=aMapSlot.loadMap";
 		  document.body.appendChild(script);
 			this.isLoaded = 1;
 		};
 	}
 	
 	this.loadMap = function(){
-		
+		console.log('meow');
 	}
 	
 }
