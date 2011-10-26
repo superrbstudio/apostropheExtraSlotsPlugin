cf1d29033136e09de57f4302938191c8ca81eec9
tboutell <tboutell@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2011-10-26 13:00:32 +0000

    Wufoo slot! We temporarily take over document.write() to get this to work in an AJAX context. That won't work for every embed code provider but it has potential for many.
    Right now we load the external javascript twice if there are two forms on the same page. That is not ideal but doesn't hurt any because Wufoo's JS is well written. TODO: track, on the client side, whether we already have the code.
    
    
    
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheExtraSlotsPlugin/trunk@4259 c89fab08-2f9b-42bf-8176-bd3ad6bee547

---
+++ lib/form/aWufooFormSlotEditForm.class.php
@@ -0,0 +1,42 @@
+<?php    
+class aWufooFormSlotEditForm extends BaseForm
+{
+  // Ensures unique IDs throughout the page
+  protected $id;
+  public function __construct($id, $defaults = array(), $options = array(), $CSRFSecret = null)
+  {
+    $this->id = $id;
+    parent::__construct($defaults, $options, $CSRFSecret);
+  }
+  public function configure()
+  {
+    $this->setWidgets(array('wufoo_code' => new sfWidgetFormTextarea()));
+    $this->setValidators(array('wufoo_code' => new sfValidatorString(array('required' => false, 'max_length' => 100))));
+    $this->widgetSchema->setHelp('wufoo_code', 'Hint: generate your form on <a href="http://wufoo.com/">wufoo.com</a>, then click "Code," then "Embed Form Code." Copy and paste the "Javascript Version" of the code here.');
+    $this->setValidators(array('wufoo_code' => new sfValidatorString(array('required' => true, 'max_length' => 5000))));
+    
+    $this->validatorSchema->setPostValidator(new sfValidatorCallback(array('callback' => array($this, 'validateWufooCode'))));
+    // Ensures unique IDs throughout the page. Hyphen between slot and form to please our CSS
+    $this->widgetSchema->setNameFormat('slot-form-' . $this->id . '[%s]');
+    
+    // You don't have to use our form formatter, but it makes things nice
+    $this->widgetSchema->setFormFormatterName('aAdmin');
+  }
+  public function validateWufooCode($validator, $values)
+  {
+    $error = false;
+    if (preg_match('/new WufooForm/', $values['wufoo_code']))
+    {
+      error_log("Matched regex");
+    }
+    else
+    {
+      $error = true;
+    }
+    if ($error)
+    {
+      throw new sfValidatorErrorSchema($validator, array('wufoo_code' => new sfValidatorError($validator, 'Hmm, that doesn\'t look like JavaScript embed form code from Wufoo.')));
+    }
+    return $values;
+  }
+}
