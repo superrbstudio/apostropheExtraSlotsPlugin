cba3f74de0b112b9d664fb1747ddf18d93cbe0fb
johnnyoffline <johnnyoffline@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2011-12-21 00:33:28 +0000

    rendered out the etsy slot form so it can be somewhat properly styled
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheExtraSlotsPlugin/trunk@4438 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aEtsySlot/templates/_editView.php
+++ modules/aEtsySlot/templates/_editView.php
@@ -1,2 +1,12 @@
 <?php // Just echo the form. You might want to render the form fields differently ?>
-<?php echo $form ?>
+<?php // echo $form ?>
+
+<div class="a-form-row etsy_code">
+	<div class="a-form-field">
+			<h4 for="<?php echo $form['etsy_code']->renderId() ?>" class="a-form-field-label">Etsy Mini Code</h4>
+    	<?php echo $form['etsy_code']->render() ?>
+      <?php echo $form['etsy_code']->renderHelp() ?>
+	</div>
+	<div class="a-form-error"><?php echo $form['etsy_code']->renderError() ?></div>
+  <?php echo $form->renderHiddenFields() ?>
+</div>
