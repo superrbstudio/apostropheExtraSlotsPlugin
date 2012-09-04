027c098f2644fae3b29183e2e48a4dcc8b6cee9e
tboutell <tboutell@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2012-09-04 18:14:14 +0000

    Ecwid slot uses SSL (not that we're using the slot, but...)
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheExtraSlotsPlugin/trunk@4817 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aEcwidProductSlot/templates/_normalViewBody.php
+++ modules/aEcwidProductSlot/templates/_normalViewBody.php
@@ -5,6 +5,10 @@
 <?php include_partial('a/simpleEditWithVariants', array('pageid' => $pageid, 'name' => $name, 'permid' => $permid, 'slot' => $slot)) ?>
 <?php $normalViewSelector = "#a-slot-content-container-$pageid-$name-$permid" ?>
 <?php if (isset($values['ecwid_code'])): ?>
+  <?php $code = $values['ecwid_code'] ?>
+  <?php if ($sf_request->isSecure()): ?>
+    <?php $code = str_replace('http://app.ecwid.com', 'https://app.ecwid.com', $code) ?>
+  <?php endif ?>
   <?php if ($sf_request->isXmlHttpRequest()): ?>
     <?php $script = json_encode($sf_request->isSecure() ? 'https://app.ecwid.com/script.js?' . $storeId : 'http://app.ecwid.com/script.js?' . $storeId) ?>
     <script type="text/javascript" charset="utf-8">
