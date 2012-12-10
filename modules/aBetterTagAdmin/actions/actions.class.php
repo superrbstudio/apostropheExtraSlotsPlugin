4a7f59fee899642cfe5c0f20c72e9a248fdad8c9
tboutell <tboutell@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2012-12-10 21:59:11 +0000

    aBetterTagAdmin is a better admin for editing tags. It's javascript-powered, no page refreshes, so you can merge a lot of tags in a hurry. I'm still doing some fine tuning, but it works. If you have the entity plugin installed it also allows you to mirror a tag to a new entity of any configured type. (TODO: a way to update that mirror later.)
    git-svn-id: http://svn.apostrophenow.org/plugins/apostropheExtraSlotsPlugin/trunk@4955 c89fab08-2f9b-42bf-8176-bd3ad6bee547

---
+++ modules/aBetterTagAdmin/actions/actions.class.php
@@ -0,0 +1,159 @@
+<?php
+
+class aBetterTagAdminActions extends sfActions
+{
+  public function executeIndex(sfWebRequest $request)
+  {
+    $sql = new aMysql();
+    // For now let's not break out tag counts for pages, because they are often
+    // virtual pages which would double count blog posts. With a fussy query I
+    // could address this. There would be a performance cost for doing so
+    $data = $sql->queryScalar('SELECT DISTINCT(ti.taggable_model) FROM tagging ti WHERE ti.taggable_model <> "aPage"');
+    $this->models = array();
+    $modelLabels = array(
+      'aMediaItem' => 'Media',
+      'aBlogPost' => 'Blog',
+      'aEvent' => 'Event'
+    );
+
+    if (class_exists('aEntityTools'))
+    {
+      $infos = aEntityTools::getClassInfos();
+      foreach ($infos as $class => $info)
+      {
+        $modelLabels[$class] = $info['singular'];
+      }
+    }
+
+    foreach ($data as $class)
+    {
+      $this->models[] = array('name' => $class, 'label' => isset($modelLabels[$class]) ? $modelLabels[$class] : $class);
+    }
+
+    $data = $sql->query('SELECT t.id as id, t.name as name, ti.taggable_model as model, COUNT(ti.taggable_id) AS count_for_model FROM tag t LEFT JOIN tagging ti ON t.id = ti.tag_id GROUP BY t.id, ti.taggable_model ORDER BY t.name');
+
+    $infoByTag = array();
+    foreach ($data as $row)
+    {
+      $infosByTag[$row['name']][$row['model']] = $row['count_for_model'];
+      $infosByTag[$row['name']]['name']= $row['name'];
+      $infosByTag[$row['name']]['id'] = $row['id'];
+    }
+    $this->tagInfos = array_values($infosByTag);
+    if (class_exists('aEntityTools'))
+    {
+      $this->classInfos = aEntityTools::getClassInfos();
+    }
+  }
+
+  public function executeDelete(sfWebRequest $request)
+  {
+    $sql = new aMysql();
+    $id = $this->getRequestParameter('id');
+    $id += 0;
+    if ($id)
+    {
+      $sql->query('DELETE FROM tagging WHERE tag_id = :id', array('id' => $id));
+      $sql->delete('tag', $id);
+      return $this->jsonResponse(array('status' => 'ok', 'deleted' => $id));
+    }
+    else
+    {
+      return $this->jsonResponse(array('status' => 'invalid'));
+    }
+  }
+
+  protected function jsonResponse($array)
+  {
+    echo(json_encode($array));
+    exit(0);
+  }
+
+  public function executeRename(sfWebRequest $request)
+  {
+    $sql = new aMysql();
+    $id = $this->getRequestParameter('id');
+    $id += 0;
+    $name = trim($this->getRequestParameter('name'));
+    if ($id && strlen($name))
+    {
+      $existing = $sql->queryOne('SELECT * FROM tag WHERE name = :name', array('name' => $name));
+      $result = array();
+      if ($existing && ($existing['id'] !== $id))
+      {
+        // Merge
+        $sql->query('UPDATE tagging SET tag_id = :existing_id WHERE tag_id = :id', array('id' => $id, 'existing_id' => $existing['id']));
+        $sql->delete('tag', $id);
+        return $this->jsonResponse(array('status' => 'ok', 'action' => 'merged', 'from' => $id, 'to' => $existing['id']));
+      }
+      else
+      {
+        $sql->query('UPDATE tag SET name = :name WHERE id = :id', array('name' => $name, 'id' => $id));
+        return $this->jsonResponse(array('status' => 'ok', 'action' => 'renamed'));
+      }
+    }
+    else
+    {
+      return $this->jsonResponse(array('status' => 'invalid'));
+    }
+  }
+
+  /**
+   * Only makes sense if you're using apostropheEntitiesPlugin
+   */
+
+  public function executeCreateEntity(sfWebRequest $request)
+  {
+    $sql = new aMysql();
+    $class = $this->getRequestParameter('className');
+    $id = $this->getRequestParameter('id');
+    $info = aEntityTools::getClassInfo($class);
+    if (!$info)
+    {
+      return $this->jsonResponse(array('status' => 'invalid'));
+    }
+    $classes = array_keys(aEntityTools::getClassInfos());
+    $tag = $sql->queryOne('SELECT * FROM tag WHERE id = :id', array('id' => $id));
+    if (!$tag)
+    {
+      return $this->jsonResponse(array('status' => 'invalid'));
+    }
+    $entity = Doctrine::getTable('aEntity')->findOneByName($tag['name']);
+    if (!$entity) 
+    {
+      $entity = new $class;
+      $entity->setName($tag['name']);
+      $entity->save();
+    }
+    if (count($classes))
+    {
+      $taggings = $sql->query('SELECT * FROM tagging WHERE tag_id = :id AND taggable_model IN :classes', array('id' => $id, 'classes' => $classes));
+      foreach ($taggings as $tagging)
+      {
+        // Entity relationships are bidirectional
+        try
+        {
+          $sql->insertOrUpdate('a_entity_to_entity', array('entity_1_id' => $tagging['taggable_id'], 'entity_2_id' => $entity['id']));
+          $sql->insertOrUpdate('a_entity_to_entity', array('entity_2_id' => $tagging['taggable_id'], 'entity_1_id' => $entity['id']));
+        } catch (Exception $e)
+        {
+          // Don't bomb if the entity no longer exists. This can happen
+          // with tags because they lack referential integrity
+        }
+      }
+    }
+    $taggings = $sql->query('SELECT * FROM tagging WHERE tag_id = :id AND taggable_model IN ("aBlogPost", "aEvent")', array('id' => $id));
+    foreach ($taggings as $tagging)
+    {
+      try
+      {
+        $sql->insertOrUpdate('a_entity_to_blog_item', array('blog_item_id' => $tagging['taggable_id'], 'entity_id' => $entity['id']));
+      } catch (Exception $e)
+      {
+        // Don't bomb if the blog post no longer exists. This can happen
+        // with tags because they lack referential integrity
+      }
+    }
+    return $this->jsonResponse(array('status' => 'ok'));
+  }
+}
---
+++ modules/aBetterTagAdmin/templates/indexSuccess.php
@@ -0,0 +1,282 @@
+<style type="text/css">
+  /* I am here to be cloned by js, not to be shown directly */
+  .template
+  {
+    display: none;
+  }
+  th, td
+  {
+    padding: 10px;
+  }
+  .editor
+  {
+    margin-right: 10px;
+  }
+  .action
+  {
+    margin-left: 10px;
+    margin-right: 10px;
+  }
+  .highlighted
+  {
+    background-color: #bfb;
+  }
+  .clickme
+  {
+    cursor: pointer;
+  }
+  .clickme:hover
+  {
+    text-decoration: underline;
+  }
+  .edit
+  {
+    display: none;
+  }
+</style>
+
+<h2 data-spinner>Loading...</h2>
+
+<table data-table style="display: none">
+  <thead>
+    <tr data-heading-row>
+      <th>Tag</th>
+      <th data-model-name-template class="template">
+      </th>
+    </tr>
+  </thead>
+  <tbody data-row-parent>
+    <tr class="template" data-row data-row-template>
+      <td class="clickme" data-tag-name data-normal></td>
+      <td data-model-count-template data-model-count data-normal class="template">0</td>
+      <?php // data-edit must be a single element ?>
+      <td colspan="2" data-edit class="edit">
+        <input type="text" name="name" class="editor" value="" data-tag-name-editor />
+        <a class="action" href="#" data-tag-save>Rename Tag</a>
+        <a class="action" href="#" data-tag-delete>Delete Tag</a>
+        <?php if ($classInfos): ?>
+          <select class="action" name="class" data-tag-entity>
+            <option value="">Create Entity</option>
+            <?php foreach ($sf_data->getRaw('classInfos') as $class => $classInfo): ?>
+              <option value="<?php echo $class ?>"><?php echo aHtml::entities($classInfo['singular']) ?></option>
+            <?php endforeach ?>
+          </select>
+        <?php endif ?>
+        <a class="action" href="#" data-tag-cancel>Cancel</a>
+      </td>
+    </tr>
+  </tbody>
+</table>
+
+<script type="text/javascript">
+  var models = <?php echo json_encode($sf_data->getRaw('models')) ?>;
+  var tags = <?php echo json_encode($sf_data->getRaw('tagInfos')) ?>;
+  var renameUrl = <?php echo json_encode(url_for('aBetterTagAdmin/rename')) ?>;
+  var deleteUrl = <?php echo json_encode(url_for('aBetterTagAdmin/delete')) ?>;
+  var createEntityUrl = <?php echo json_encode(url_for('aBetterTagAdmin/createEntity')) ?>;
+
+  var tagAdmin = {
+    rebuild: function() {
+      var spinner = $('[data-spinner]');
+      spinner.show();
+      var table = $('[data-table]');
+      table.hide();
+      var headingRow = table.find('[data-heading-row]');
+      var modelNameTemplate = table.find('[data-model-name-template]').clone();
+      var dataRowTemplate = table.find('[data-row-template]').clone();
+      var modelCountTemplate = dataRowTemplate.find('[data-model-count-template]').clone();
+      
+      // For DOM performance reasons we'll add the edit controls only as needed
+      var editControlsTemplate = dataRowTemplate.find('[data-edit]').clone();
+      dataRowTemplate.find('[data-edit]').remove();
+
+      // Mop up sub-templates now that we've cloned them. This way we don't
+      // keep cloning them when we make data rows
+      dataRowTemplate.find('.template').remove();
+
+      _.each(models, function(model) {
+        var heading = modelNameTemplate.clone();
+        heading.removeClass('template').text(model.label);
+        headingRow.append(heading);
+      });
+
+      _.each(tags, function(tag) {
+        var dataRow = dataRowTemplate.clone();
+        dataRow.attr('data-tag-id', tag.id);
+        dataRow.attr('data-tag-name', tag.name);
+        dataRow.removeClass('template');
+        if (tag.name === '')
+        {
+          dataRow.find('[data-tag-name]').text('[NO NAME]');
+        }
+        else
+        {
+          dataRow.find('[data-tag-name]').text(tag.name);
+        }
+        _.each(models, function(model) {
+          var modelCount = modelCountTemplate.clone();
+          modelCount.removeClass('template');
+          if (_.has(tag, model.name))
+          {
+            modelCount.text(tag[model.name]);
+          }
+          dataRow.append(modelCount);
+        });
+        // We don't want this to get show()n later
+        table.find('[data-row-parent]').append(dataRow);
+      });
+
+      spinner.hide();
+      table.show();
+
+      // EVENT HANDLERS AND PRIVATE FUNCTIONS FOLLOW
+
+      var activeRow;
+
+      // Toggle open the editor for a row
+      $('[data-row]').click(function() {
+        // Don't interfere with a click on the thing we're already editing
+        if (activeRow && ($(this).attr('data-tag-id') === activeRow.attr('data-tag-id')))
+        {
+          return true;
+        }
+        hideControls();
+        activeRow = $(this);
+        showControls();
+      });
+
+      function hideControls() {
+        if (!activeRow) {
+          return;
+        }
+        activeRow.find('[data-edit]').remove();
+        activeRow.find('[data-normal]').show();
+        activeRow = undefined;
+      }
+
+      function showControls() {
+        unhighlight();
+        if (!activeRow) {
+          return;
+        }
+        activeRow.find('[data-normal]').hide();
+        activeRow.append(editControlsTemplate.clone());
+        activeRow.find('[data-tag-name-editor]').val(activeRow.find('[data-tag-name]').text());
+        enableControls();
+        activeRow.find('[data-edit]').show();
+      }
+
+      function enableControls() {
+        activeRow.find('[data-tag-save]').click(function() {
+          var button = $(this);
+          var id = activeRow.attr('data-tag-id');
+          var editor = activeRow.find('[data-tag-name-editor]');
+          var name = editor.val();
+          $.post(renameUrl, { id: id, name: name }, function(data) {
+            if (data.status === 'ok') {
+              if (data.action === 'merged') {
+                activeRow.fadeOut();
+                mergeCounts(id, data.to);
+                highlight(data.to);
+              } else {     
+                // Reinsert the renamed row alphabetically by performing a
+                // simple insertion sort      
+                activeRow.find('[data-tag-name]').text(name);
+                activeRow.remove();
+                var i;
+                var found = false;
+                for (i = 0; (i < tags.length); i++) {
+                  if (tags[i].name.toLowerCase() > name.toLowerCase()) {
+                    var row = findRow(tags[i].id);
+                    row.before(activeRow);
+                    found = true;
+                    break;
+                  }
+                }
+                if (!found) {
+                  table.append(activeRow);
+                }
+                highlight(id);
+              }
+            }
+            hideControls();
+          }, 'json');
+          return false;
+        });
+
+        activeRow.find('[data-tag-delete]').click(function() {
+          if (!confirm('Are you sure you want to delete this tag?'))
+          {
+            return false;
+          }
+          var button = $(this);
+          var id = activeRow.attr('data-tag-id');
+          $.post(deleteUrl, { id: id }, function(data) {
+            if (data.status === 'ok') {
+              activeRow.remove();
+            }
+          }, 'json');
+          return false;
+        });
+
+        activeRow.find('[data-tag-cancel]').click(function() {
+          hideControls();
+          return false;
+        });
+
+        activeRow.find('[data-tag-entity]').change(function() {
+          var id = activeRow.attr('data-tag-id');
+          var className = activeRow.find('[data-tag-entity]').val();
+          $.post(createEntityUrl, { id: id, className: className }, function(data) {
+            if (data.status === 'ok') {
+              hideControls();
+              highlight(id);
+            }
+          }, 'json');
+        });
+      }
+
+      function findRow(id) {
+        return table.find('[data-tag-id="' + id + '"]');
+      }
+
+      function mergeCounts(from, to) {
+        var fromRow = findRow(from);
+        var toRow = findRow(to);
+
+        var fromCounts = fromRow.find('[data-model-count]');
+        var toCounts = toRow.find('[data-model-count]');
+        var i;
+        for (i = 0; (i < fromCounts.length); i++) {
+          $(toCounts[i]).text(parseInt($(toCounts[i]).text()) + parseInt($(fromCounts[i]).text()));
+        }
+      }
+
+      var lastHighlight;
+
+      function highlight(id) {
+        unhighlight();
+        var row = findRow(id);
+        row.addClass('highlighted');
+
+        // jquery.scrollTo was failing, putting the row just above
+        // the screen, not very useful. This works pretty well
+        // and requires no plugins
+        var top = Math.max(row.offset().top - 100, 0);
+        $('body').scrollTop(top);
+
+        lastHighlight = id;
+      }
+
+      function unhighlight() {
+        if (lastHighlight) {
+          var row = findRow(lastHighlight);
+          row.removeClass('highlighted');
+        }
+      }
+    }
+  };
+
+  tagAdmin.rebuild();
+</script>
+
